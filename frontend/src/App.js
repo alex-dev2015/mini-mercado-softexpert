429a5c7adc1adbb15863537fcc7e8f8e4a202ddb
alexdev20 <alex.sousa@innyx.com>
2023-06-04 19:15:25 -0300

    Implementando tela de Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,73 @@
-import React, { Component } from 'react';
-import AdminLTE, { Sidebar } from 'adminlte-2-react';
+import React, {Component, useEffect, useState} from 'react';
+import AdminLTE, {Sidebar} from 'adminlte-2-react';
 
+import Login from "./pages/Login";
 import Home from './pages/Home';
 import Sale from "./pages/Sale";
 import Produto from "./pages/Produto";
 import TipoProduto from "./pages/TipoProduto";
 
 import UsuarioImg from './assets/image/user.png'
+import api from "./services/api";
 
-const { Item, UserPanel } = Sidebar;
+const {Item, UserPanel} = Sidebar;
 
-class App extends Component {
+const App = () => {
 
-  sidebar = [
-    <UserPanel username='Alex Nascimento' key="username" imageUrl={UsuarioImg} status='online' />,
-    <Item key="home" text="Home" to="/home" />,
-    <Item icon='fa-inbox' key="tipo_produtos" text="Tipo de Produtos" to="/tipo_produtos" />,
-    <Item icon='fa-box' key="produtos" text="Produtos" to="/produtos" />,
-    <Item icon='fa-shopping-cart' key="sales" text="Vendas" to="/sales" />,
-  ]
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [user, setUser] = useState('');
 
-  render() {
+
+    useEffect(() => {
+        const authSession = 'Bearer '+sessionStorage.getItem('session');
+        api.get('authenticate', {
+            headers: {
+                "Authorization" : authSession
+            }
+        }).then((result) => {
+            const loggedIn = sessionStorage.getItem('login') === 'true';
+            setUser(result.data.email);
+            setIsAuthenticated(loggedIn);
+        }).catch((error) => {
+            handleLogout();
+        })
+    }, []);
+
+    const handleLogin = () => {
+        sessionStorage.setItem('login', 'true');
+        setIsAuthenticated(true);
+    };
+
+    const handleLogout = () => {
+        sessionStorage.setItem('login', 'false');
+        sessionStorage.removeItem('session');
+        setIsAuthenticated(false);
+    };
+
+    const sidebar = [
+        <UserPanel username={user} key="username" imageUrl={UsuarioImg} status='online'/>,
+        <Item key="home" text="Home" to="/home"/>,
+        <Item icon='fa-inbox' key="tipo_produtos" text="Tipo de Produtos" to="/tipo_produtos"/>,
+        <Item icon='fa-box' key="produtos" text="Produtos" to="/produtos"/>,
+        <Item icon='fa-shopping-cart' key="sales" text="Vendas" to="/sales"/>,
+        <Item icon='fa-sign-out' key="logout" text="Logout" to="/login" onClick={handleLogout} />,
+    ]
+
     return (
-      <AdminLTE title={["Soft", "Expert"]} titleShort={["He", "we"]} theme="red" sidebar={this.sidebar}>
-        <Home path="/home" />
-        <Produto path="/produtos" />
-        <Sale path="/sales" />
-        <TipoProduto path="/tipo_produtos" />
-      </AdminLTE>
+        <div className="wrapper">
+            {isAuthenticated ? (
+                <AdminLTE title={['Soft', 'Expert']} titleShort={['He', 'we']} theme="red" sidebar={sidebar}>
+                    <Home path="/home"/>
+                    <Produto path="/produtos"/>
+                    <Sale path="/sales"/>
+                    <TipoProduto path="/tipo_produtos"/>
+                </AdminLTE>
+            ) : (
+                <Login onLogin={handleLogin}/>
+            )}
+        </div>
     );
-  }
+
 }
 
 export default App;
